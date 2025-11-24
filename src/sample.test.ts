3043e40d6c5f1f1bb8850525cb37a2e9b2d5ad25
Diogo Azevedo <diogoazevedos@gmail.com>
2025-11-24 10:13:14 +0100

    chore: use type stripping

--- src/sample.test.ts
+++ src/sample.test.ts
@@ -1,6 +1,6 @@
 import test from 'node:test';
 import assert from 'node:assert/strict';
-import {sayHello} from './sample.js';
+import {sayHello} from './sample.ts';
 
 void test('should say hello to given string', () => {
 	assert.equal(sayHello('World'), 'Hello World!');
